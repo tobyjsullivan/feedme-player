75174bda87925b9d58c6722420015f9ba19c5a9f
Toby Sullivan <tobyjsullivan@gmail.com>
2014-03-15 17:27:01 -0700

    Changed player path and formatted better for embedding

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,33 @@
 package controllers
 
+import scala.util._
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws.WS
 
 object Application extends Controller {
-  
-  def index(filepath: String) = Action {
-    Ok(views.html.index(filepath))
-  }
-  
+
+    def index = Action { request =>
+        val hostname = request.host
+
+        Ok(views.html.index(hostname))
+    }
+
+    def player(resource: String) = Action {
+        val filepath = "http://bit.ly/" + resource
+
+        Async {
+            val fResp = WS.url(filepath).withFollowRedirects(false).head
+
+            fResp.map { resp =>
+                println("Status: "+resp.status)
+                println(resp.ahcResponse.getHeaders())
+                val resolved = resp.header("Location").getOrElse(filepath)
+                Ok(views.html.player(resolved))
+            }
+        }
+    }
+
 }
